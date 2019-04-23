8f913c3186b37b9627deba138e02669b492d4231
sqarrt <sqarrt1337@gmail.com>
2019-04-23 18:04:36 +0300

    started to learn. saving and getting weights

--- learn_to_play.py
+++ learn_to_play.py
@@ -1,7 +1,12 @@
 import numpy
+
+import os
+os.environ['KERAS_BACKEND'] = 'theano'
+
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.utils import np_utils
+from keras import optimizers
 import csv
 
 # Устанавливаем seed для повторяемости результатов
@@ -22,8 +27,8 @@ def load_data():
             in_data = in_data.replace('_', '0')
             in_data = in_data.replace('-', '0')
 
-            out_data = out_data.replace('X', '1')
-            out_data = out_data.replace('+', '1')
+            out_data = out_data.replace('X', '0')
+            out_data = out_data.replace('+', '0')
             out_data = out_data.replace('O', '1')
             out_data = out_data.replace('_', '0')
             out_data = out_data.replace('-', '0')
@@ -43,15 +48,10 @@ def load_data():
     output_data_test = output_data[8000:]
     output_data = output_data[:8000]
 
-    (input_data, output_data), (input_data_test, output_data_test)
-
     return (input_data, output_data), (input_data_test, output_data_test)
-
 
 # Загружаем данные
 (X_train, y_train), (X_test, y_test) = load_data()
-
-print('\n')
 
 # # Преобразование размерности изображений
 # X_train = X_train.reshape(60000, 784)
@@ -61,26 +61,43 @@ print('\n')
 # X_test = X_test.astype('float32')
 # X_train /= 255
 # X_test /= 255
+
+# Создаем последовательную модель
+model = Sequential()
+
+# Добавляем уровни сети
+model.add(Dense(100, input_dim=100, activation="softsign", kernel_initializer="normal"))
+model.add(Dense(20, activation="softsign", kernel_initializer="normal"))
+model.add(Dense(10, activation="softsign", kernel_initializer="normal"))
+model.add(Dense(100, activation="softsign", kernel_initializer="normal"))
+
+# Компилируем модель
+model.compile(loss="mean_absolute_error", optimizer=optimizers.SGD(lr=0.4, clipvalue=0.5), metrics=["mse"])
+
+print(model.summary())
+
+# Обучаем сеть
+model.fit(X_train, y_train, batch_size=200, epochs=70, validation_split=0.2, verbose=2)
+
+# Оцениваем качество обучения сети на тестовых данных
+scores = model.evaluate(X_test, y_test, verbose=0)
+print("Точность работы на тестовых данных: %.2f%%" % (scores[1]*100))
+
+print("Сохраняем сеть")
+# Сохраняем сеть для последующего использования
+# Генерируем описание модели в формате json
+model_json = model.to_json()
+json_file = open("seabattle_model.json", "w")
+# Записываем архитектуру сети в файл
+json_file.write(model_json)
+json_file.close()
+# Записываем данные о весах в файл
+model.save_weights("seabattle_model.h5")
+print("Сохранение сети завершено")
+
+# res = model.predict(X_test)
 #
-# # Преобразуем метки в категории
-# Y_train = np_utils.to_categorical(y_train, 10)
-# Y_test = np_utils.to_categorical(y_test, 10)
+# res = zip(X_test[0], y_test[0], [1 if a >= 0.2 else 0 for a in res[0]])
 #
-# # Создаем последовательную модель
-# model = Sequential()
-#
-# # Добавляем уровни сети
-# model.add(Dense(800, input_dim=784, activation="relu", kernel_initializer="normal"))
-# model.add(Dense(10, activation="softmax", kernel_initializer="normal"))
-#
-# # Компилируем модель
-# model.compile(loss="categorical_crossentropy", optimizer="SGD", metrics=["accuracy"])
-#
-# print(model.summary())
-#
-# # Обучаем сеть
-# model.fit(X_train, Y_train, batch_size=200, epochs=25, validation_split=0.2, verbose=2)
-#
-# # Оцениваем качество обучения сети на тестовых данных
-# scores = model.evaluate(X_test, Y_test, verbose=0)
-# print("Точность работы на тестовых данных: %.2f%%" % (scores[1]*100))
+# for a in res:
+#     print(a)
